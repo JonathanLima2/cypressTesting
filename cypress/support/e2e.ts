f1a05b3f1b6fb049e4850c2b992690cfbaf23b61
JonathanLima2 <jdsl@icomp.ufam.edu.br>
2024-11-04 10:44:23 -0400

    add: fixed mochawesome setup and generated report

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,5 +1,2 @@
-// import 'cypress-mochawesome-reporter';
+import 'cypress-mochawesome-reporter/register';
 import './commands';
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
